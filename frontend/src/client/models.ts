5763f0f8691209c1660b51137f56ef75ac7259ff
Sy Tran Dung <sytrancsvn@gmail.com>
2024-10-26 17:12:23 +0700

    add generate tool

---
+++ frontend/src/client/models.ts
@@ -0,0 +1,112 @@
+export type Body_login_login_access_token = {
+	grant_type?: string | null;
+	username: string;
+	password: string;
+	scope?: string;
+	client_id?: string | null;
+	client_secret?: string | null;
+};
+
+
+
+export type HTTPValidationError = {
+	detail?: Array<ValidationError>;
+};
+
+
+
+export type Message = {
+	message: string;
+};
+
+
+
+export type NewPassword = {
+	token: string;
+	new_password: string;
+};
+
+
+
+export type Role = 'CANDIDATE' | 'EXAMINER';
+
+
+
+export type Token = {
+	access_token: string;
+	token_type?: string;
+};
+
+
+
+export type UpdatePassword = {
+	current_password: string;
+	new_password: string;
+};
+
+
+
+export type UserCreate = {
+	email: string;
+	is_active?: boolean;
+	is_superuser?: boolean;
+	role?: Role;
+	full_name?: string | null;
+	password: string;
+};
+
+
+
+export type UserPublic = {
+	email: string;
+	is_active?: boolean;
+	is_superuser?: boolean;
+	role?: Role;
+	full_name?: string | null;
+	id: string;
+};
+
+
+
+export type UserRegister = {
+	email: string;
+	password: string;
+	full_name?: string | null;
+};
+
+
+
+export type UserUpdate = {
+	email?: string | null;
+	is_active?: boolean;
+	is_superuser?: boolean;
+	role?: Role;
+	full_name?: string | null;
+	password?: string | null;
+};
+
+
+
+export type UserUpdateMe = {
+	email?: string | null;
+	is_active?: boolean;
+	is_superuser?: boolean;
+	role?: Role;
+	full_name?: string | null;
+};
+
+
+
+export type UsersPublic = {
+	data: Array<UserPublic>;
+	count: number;
+};
+
+
+
+export type ValidationError = {
+	loc: Array<string | number>;
+	msg: string;
+	type: string;
+};
+
