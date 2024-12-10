f2445f6b98c432a10cbed520f0201c6458195d08
Sy Tran Dung <sytrancsvn@gmail.com>
2024-12-10 22:14:26 +0700

    fix api

--- backend/app/api/routes/registered_exams.py
+++ backend/app/api/routes/registered_exams.py
@@ -1,14 +1,17 @@
 import os
-from typing import Any
+from typing import Any, List
 import uuid
 
-from fastapi import APIRouter, BackgroundTasks, HTTPException, UploadFile, status
-from sqlmodel import Session, select
+from fastapi import APIRouter, BackgroundTasks, HTTPException, UploadFile
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import SessionTransaction
+from sqlmodel import select
 
-from app.api.deps import MinioDep, SessionDep, CurrentUser, get_db, get_minio_client
-from app.models import CandidateExam, CandidateExamEssay, CandidateExamStatus, Exam, Part, Question, Skill
-from app.services.ai_service import AssessmentResult
+from app.api.deps import MinioDep, SessionDep, CurrentUser
+from app.models import CandidateExam, CandidateExamAnswer, CandidateExamEssay, CandidateExamStatus, Exam, Part, Question, Skill
+from app.services.ai_service import AssessmentResult, assess_speaking_essay, assess_writing_essay, transcribe_file
 from app.view_models import (
+    AnswerIn,
     ExamFinished,
     ExamReadonly,
     ExamSubmit,
@@ -64,9 +67,10 @@ def read_exam_result(session: SessionDep,
     return ExamFinished(**exam.model_dump(), score=score + essay_score)
 
 
-@router.post("/{id}/answers", response_model=RegisteredExamPublic)
-def add_answer(session: SessionDep,
+@router.post("/{id}/answers", response_model=List[AnswerIn])
+def add_answers(session: SessionDep,
                current_user: CurrentUser,
+               answers_in: List[AnswerIn],
                id: uuid.UUID) -> Any:
     """
     Add an answer.
@@ -74,31 +78,26 @@ def add_answer(session: SessionDep,
     exam = session.get(CandidateExam, id)
     if not exam:
         raise HTTPException(status_code=404, detail="Exam not found")
-    return exam
-
-
-def speech_to_text(file):
-    pass
-
-
-def upload_speaking_essay(candidate_exam_id: uuid.UUID, question_id: uuid.UUID, file):
-    from app.core.db import engine
-    minio = get_minio_client()
-    with Session(engine) as session:
-        text = speech_to_text(file)
-        essay = session.exec(
-            select(CandidateExamEssay)
-            .where(CandidateExamEssay.candidate_exam_id == candidate_exam_id,
-                   CandidateExamEssay.question_id == question_id)
-        ).first()
-        _, fext = os.path.splitext(file.filename or "")
-        stored_file_name = f"{candidate_exam_id}/{question_id}/{essay.id}{fext}"
-        minio.put_object(f"vrun",
-                         stored_file_name,
-                         file.file, file.size or -1)
-        essay.content = text
-        essay.resource = stored_file_name
+    answers = [CandidateExamAnswer(candidate_exam_id=exam.id, **a.model_dump()) for a in answers_in]
+    try:
+        for a in exam.selected_answers:
+            session.delete(a)
+        session.add_all(answers)
         session.commit()
+        session.refresh(exam)
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="Invalid answers")
+    return exam.selected_answers
+
+
+async def upload_speaking_essay(minio, candidate_exam_id: uuid.UUID, question_id: uuid.UUID, file):
+    text = await transcribe_file(file)
+    _, fext = os.path.splitext(file.filename or "")
+    stored_file_name = f"{candidate_exam_id}/{question_id}{fext}"
+    minio.put_object("vrun",
+                     stored_file_name,
+                     file.file, file.size or -1)
+    return text or "", stored_file_name
 
 
 @router.post("/{id}/essays", response_model=RegisteredExamPublic)
@@ -106,8 +105,8 @@ def add_speaking_record(session: SessionDep,
                         current_user: CurrentUser,
                         minio: MinioDep,
                         id: uuid.UUID,
-                        essay_in: EssayIn,
                         file: UploadFile | None,
+                        essay_in: EssayIn,
                         background_tasks: BackgroundTasks):
 
     exam = session.exec(
@@ -122,6 +121,9 @@ def add_speaking_record(session: SessionDep,
     if not question:
         raise HTTPException(status_code=404, detail="Question not found")
 
+    if question.question_group.skill not in (Skill.WRITING, Skill.SPEAKING):
+        raise HTTPException(status_code=400, detail="Not a writing or speaking question")
+
     if question.question_group.skill == Skill.SPEAKING:
         if file is None:
             raise HTTPException(status_code=400, detail="Speaking essay must be audio")
@@ -132,12 +134,14 @@ def add_speaking_record(session: SessionDep,
             raise HTTPException(status_code=400, detail="Invalid file type, please upload mp3 file")
         if file.size is None:
             raise HTTPException(status_code=400, detail="Invalid file size")
-        if file.size > 3 * 2**20:
+        if file.size > 100 * 2**20:
             raise HTTPException(status_code=400, detail="File too large.")
 
         essay = CandidateExamEssay.model_validate(dict(**essay_in.model_dump()))
         session.add(essay)
-        background_tasks.add_task(upload_speaking_essay, id, essay_in.question_id, file)
+        content, resource = upload_speaking_essay(minio, id, essay_in.question_id, file)
+        essay.content = content
+        essay.resource = resource
     else:
         essay = CandidateExamEssay.model_validate(essay_in)
         session.add(essay)
@@ -147,19 +151,20 @@ def add_speaking_record(session: SessionDep,
     return exam
 
 
+
 def grade_exam(session: SessionDep, candidate_exam: CandidateExam):
-    from app.services.ai_service import assess_writing_essay
-
-    # get essays
     for essay in candidate_exam.essays:
+        # TODO: status between user added essay but not completed processing
         if essay.question.question_group.skill == Skill.SPEAKING:
-            # TODO: convert speech to text and store to content
-            pass
-
-        result: AssessmentResult = assess_writing_essay(
-            essay.question.description,
-            essay.content
-        )
+            result: AssessmentResult = assess_speaking_essay(
+                essay.question.description,
+                essay.content
+            )
+        else:
+            result: AssessmentResult = assess_writing_essay(
+                essay.question.description,
+                essay.content
+            )
         essay.score = result.score.summary
         essay.assetsment = str(result.assessment)
 
