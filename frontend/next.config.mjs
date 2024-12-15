a294487543a8faf13ce5ee6087bf21193b604f2e
Sy Tran Dung <sytrancsvn@gmail.com>
2024-12-15 16:45:04 +0700

    frontend-docker

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone'
   // publicRuntimeConfig: {
   //   baseUrl: process.env.NODE_ENV == 'development'
   //     ? 'http://localhost:3000'
